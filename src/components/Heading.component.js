adda288a6ce51aafbd82589998840c9d4bbf30ab
mco <muslumcanozata@gmail.com>
2024-03-10 19:28:55 +0300

    initializing component for heading

---
+++ src/components/Heading.component.js
@@ -0,0 +1,35 @@
+import { Button, Flex, Heading, Highlight } from "@chakra-ui/react";
+import React from "react";
+import { useModalStore } from "../stores/model.store";
+
+const HeadingComponent = ({ title }) => {
+  const titleSplit = title.split(" ");
+  const { onOpen } = useModalStore();
+
+  return (
+    <Flex align={"center"} justify={"space-between"}>
+      <Heading lineHeight="tall">
+        > {titleSplit[0]}
+        {titleSplit.length > 1 && (
+          <Highlight
+            query={titleSplit[1]}
+            styles={{
+              px: "2",
+              py: "1",
+              rounded: "full",
+              bg: "teal.400",
+              color: "white",
+            }}
+          >
+            {titleSplit[1]}
+          </Highlight>
+        )}
+      </Heading>
+      <Button onClick={() => onOpen(null)} colorScheme={"teal"}>
+        Add Note
+      </Button>
+    </Flex>
+  );
+};
+
+export default HeadingComponent;
